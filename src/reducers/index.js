a034f08b36d2caacdcc17a37a97b3adf0f2fc402
vivshaw <hvivianshaw@gmail.com>
2017-09-18 03:05:09 -0400

    remove unused test data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,6 @@ import { combineReducers } from 'redux';
 
 import { GET_CATEGORIES } from '../actions';
 
-const allCategories = [1, 2, 3];
-
 const categories = (state = [], action) => {
 	switch (action.type) {
 		case GET_CATEGORIES:
