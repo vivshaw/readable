d8fb46b732c01286a771dde32c1d56198f950968
vivshaw <hvivianshaw@gmail.com>
2017-09-25 02:24:29 -0400

    optimistify voting on posts

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,6 +1,12 @@
 import omit from 'lodash/omit';
 
-import { RECEIVE_POSTS, DELETE_POST, EDIT_POST } from '../actions';
+import {
+	RECEIVE_POSTS,
+	DELETE_POST,
+	EDIT_POST,
+	UPVOTE,
+	DOWNVOTE
+} from '../actions';
 
 const posts = (state = {}, action) => {
 	const { id, posts, changes } = action;
@@ -11,6 +17,12 @@ const posts = (state = {}, action) => {
 		case EDIT_POST:
 			const editedPost = Object.assign({}, state[id], changes);
 			return { ...state, [id]: editedPost };
+		case UPVOTE:
+			const upvotedScore = state[id].voteScore + 1;
+			return { ...state, [id]: { ...state[id], voteScore: upvotedScore } };
+		case DOWNVOTE:
+			const downvotedScore = state[id].voteScore - 1;
+			return { ...state, [id]: { ...state[id], voteScore: downvotedScore } };
 		case DELETE_POST:
 			return omit(state, action.payload);
 		default:
